bf16c7f54511df1330c87ffff01214c42449bebb
ktrin-u <kdtrinidad2@up.edu.ph>
2025-04-13 15:55:42 +0800

    feat: add view-only searchbox

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { SearchBoxComponent } from "./search-box/search-box.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: "app-root",
+    imports: [SearchBoxComponent],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  title = 'web';
-}
+export class AppComponent {}
